8474d97d006b9a0de0f35839b9cf3b6b384c476b
Darren Trang <darrentrang93@gmail.com>
2017-05-13 12:29:33 -0700

    cleaning code

--- weather.js
+++ weather.js
@@ -1,9 +1,9 @@
-module.exports = function (callback) { //built into Node js and can set to whatever u want. in example-callback.js, you pass weather() a function which is named "callback". so in here you just invoke callback() which all it does is console.log
-	var request = require('request');
-	var encodedLocation = "Corona, CA";
-	var url = 'http://api.openweathermap.org/data/2.5/weather?appid=a0580244d8160ccfe3836f6d7b6c633b&q=' + encodedLocation + '&units=imperial';
-	console.log(url);
-	
+var request = require('request');
+var encodedLocation = "Corona, CA";	
+var url = 'http://api.openweathermap.org/data/2.5/weather?appid=a0580244d8160ccfe3836f6d7b6c633b&q=' + encodedLocation + '&units=imperial';
+//console.log(url);
+
+module.exports = function (callback) { //built into Node js and can set to whatever u want. in example-callback.js, you pass weather() a function which is named "callback". so in here you just invoke callback() which all it does is console.log	
 	request({url: url, json: true}, 
 	function(error, response, body) { //error will be null if none
 		if(error){
